a61b2a72798268d8c56604847d535470309ff4d6
Rodrigues Anthony <anthony-rodrigues31@hotmail.fr>
2025-02-16 16:22:28 +0100

    Ajout auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/plantes-interieures", "Plantes interieures", "/pages/plantes-interieures.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
 
 ];
 
